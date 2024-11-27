9648c0d4d77f8ab4a1643e2fb3d9b4d9f72be666
Ulrich <ulrich.kraus@jcl.de>
2024-11-27 20:29:43 +0100

    Update streamlit_app.py
    with json

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,4 +34,5 @@ if ingredients_list: #is not null:
 
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+#st.text(smoothiefroot_response)
+st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
